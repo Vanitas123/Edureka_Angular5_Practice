f4438c051bcf2ba1ca1d4f8a0d6374c49e050326
Vanita Sutar <vanita.sutar@CALSOFTHQ.COM>
2018-02-23 19:04:10 +0530

    Added componets for practice - header and footer

---
+++ src/layout/mainheader.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+@Component({
+    selector:'main-header',
+    templateUrl:'./mainheader.html'
+})
+
+export class MainHeaderComponent{
+    
+}
