cd6189857e718fd3367ffe1c30abf0a4e3ce8d99
Vanita Sutar <vanita.sutar@CALSOFTHQ.COM>
2018-02-27 16:20:14 +0530

    Code to create productFilter and toggle between List View/Grid View

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,27 @@
 import {NgModule} from '@angular/core';
 import {MyFirstComponent} from './app.component';
+import { ProductComponent } from '../product/product.component';
 import {BrowserModule} from '@angular/platform-browser';
+import { MainHeaderComponent } from '../layout/mainheader.component';
+import { MainFooterComponent } from '../layout/mainfooter.component';
+import {FormsModule} from '@angular/forms';
+import { ProductFilter } from '../product/product.filter.pipe';
 
 @NgModule({
-imports:[BrowserModule],
-declarations:[MyFirstComponent],
-bootstrap:[MyFirstComponent]
+    imports:[
+        BrowserModule,
+        FormsModule
+    ],
+    declarations:[
+        MyFirstComponent,
+        ProductComponent,
+        MainHeaderComponent,
+        MainFooterComponent,
+        ProductFilter
+    ],
+    bootstrap:[
+        MyFirstComponent
+    ]
 })
 
 export class AppModule{}
